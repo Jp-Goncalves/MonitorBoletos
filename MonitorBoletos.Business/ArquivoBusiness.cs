79f8d43c28ba98da564a10a25a5dc63c7ceadc89
João <jp.capucho@hotmail.com>
2018-02-16 18:43:34 -0200

    Adicionado Regex para ler o cabeçalho do arquivo de Cnab

--- MonitorBoletos.Business/ArquivoBusiness.cs
+++ MonitorBoletos.Business/ArquivoBusiness.cs
@@ -3,9 +3,11 @@ using MonitorBoletos.DAO;
 using MonitorBoletos.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace MonitorBoletos.Business
 {
@@ -18,14 +20,43 @@ namespace MonitorBoletos.Business
 
         public ArquivoBusiness()
         {
-            
+
         }
 
         public bool validarArquivo(string filename)
         {
-            //TODO : Criar rotina para validar aequivo
+            //TODO : Criar rotina para validar arquivo
 
-            return true;
+            var result = filename;
+            var extensao = Path.GetExtension(result).ToUpper();
+            var ext = ".RET";
+
+            if (filename == null)
+            {
+                return false;
+            }
+            else
+            {
+                if (extensao != ext)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public void validarArquivoRetorno(string file)
+        {
+            //TODO : Validar a Empresa, carteira e banco do arquivo
+
+            //Regex para ler o Header do arquivo
+            var leitorRegularExpression = Regex.Split(file,
+                @",(^\d{1}\d{1}[A-Z]{7}\d{2}[\s A-Z\s]{15}\d{20}[\s A-Z\s]{30}\d{3}[\s A-Z \s]{15}\d{6}\d{8}\d{5}.{266}\d{6}.{9}\d{6})", RegexOptions.Compiled);
+
+            var banco = new Banco();
+
+            banco.Numero = leitorRegularExpression[7].ToString();
+            banco.Nome = leitorRegularExpression[8].ToString().ToUpper();
         }
 
         public Cronn.Core.Tendencia.Model.Cobranca obterBoletoCronn(OcorrenciaCobranca ocorrencia)
@@ -44,7 +75,7 @@ namespace MonitorBoletos.Business
 
         public void processarLinhas(string filename)
         {
-                        
+
         }
 
     }
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String arquivo { get; set; }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = ".RET";
+            openFile.Title = "Selecio o arquivo";
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                txtEnviar.Text = openFile.FileName;
+            }
+                arquivo = openFile.FileName;
+            var validarArquivo = new MonitorBoletos.Business.ArquivoBusiness();
+            validarArquivo.validarArquivo(arquivo);
+        }
+    }
+}
